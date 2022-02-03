0cefe286bc242af939b88a63a5203bc3e23393d0
ToxicSlime <phatasmiccarnival@gmail.com>
2022-02-03 07:58:25 -0500

    really odd thing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Yeah this is a regular calculator.. nothing real special about this
+had to update due to some odd behavior
--- simple_math.c
+++ simple_math.c
@@ -24,31 +24,9 @@ int main() {
     printf("Addition: %.3lf\n",answer);
     printf("Subtraction: %.3lf\n",subanswer);
     printf("Multiplication: %.3lf\n",multianswer);
-    printf("Division: %.3lf\n",divan#include <stdio.h>
-#include <stdlib.h>
+    printf("Division: %.3lf\n",divanswer);
 
-int main() {
-    double userinput;
-    double userinput2;
-    double answer;
-    double subanswer;
-    double multianswer;
-    double divanswer;
-    char goodjob;
-    printf("enter a number: \n");
-    scanf("%lf", &userinput);
-    
-    printf("enter another number: \n");
-    scanf("%lf", &userinput2);
-
-    answer = userinput + userinput2;
-    subanswer = userinput - userinput2;
-    multianswer = userinput * userinput2;
-    divanswer = userinput / userinput2;
-    goodjob = getchar();
-
-
-};
+    printf("did i do a goodjob?\n");
     printf("y or no\n");
     scanf("%c", &goodjob);
 
@@ -60,3 +38,4 @@ int main() {
     }
     return 0;
 }
+
